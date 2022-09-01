0119cf6e9726afdc91f68091b4c75113682cc001
Yago Castro <yago.fanstour@gmail.com>
2022-09-01 17:52:11 -0300

    entrega requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-
-
 const paleta = document.querySelectorAll('.color');
 const btnColor = document.getElementById('button-random-color');
-const pixels = document.getElementsByClassName('.pixel');
+const pixels = document.querySelectorAll('.pixel');
+const boardPix = document.getElementById('pixel-board');
+const btnLimpa = document.getElementById('clear-board');
+const black = document.getElementById('black');
+const brown = document.getElementById('brown');
+const yellow = document.getElementById('yellow');
+const orange = document.getElementById('orange');
 
 // let storage = localStorage;
 // let salvaCor = JSON.parse(storage.getItem('colorPallet'));
@@ -13,14 +17,17 @@ const corDois = paleta[1];
 const corTres = paleta[2];
 const corQua = paleta[3];
 
-function classInicia () {
+black.style.backgroundColor = 'black'
+
+backgroundPalets()
+
+function classInicia() {
   corUm.classList.add('selected');
 
   return classInicia;
-};
+}
 
 window.onload = classInicia();
-
 
 function corAleatória() {
   const r = parseInt(Math.random() * 255, 0);
@@ -53,13 +60,12 @@ btnColor.addEventListener('click', () => {
 // };
 
 function trocaClass(e) {
-    for (let i = 0; i < paleta.length; i += 1) {
-      if (paleta[i].className === 'color selected') {
-        paleta[i].className = 'color';
-      }
+  for (let i = 0; i < paleta.length; i += 1) {
+    if (paleta[i].className === 'color selected') {
+      paleta[i].className = 'color';
     }
-        e.target.classList.add('selected');
-
+  }
+  e.target.classList.add('selected');
 }
 
 corUm.addEventListener('click', trocaClass);
@@ -68,3 +74,34 @@ corTres.addEventListener('click', trocaClass);
 corQua.addEventListener('click', trocaClass);
 
 
+
+function preenche(e) {
+  if (black.className === 'color selected') {
+  e.target.style.backgroundColor = black.style.backgroundColor;
+  }
+  if (brown.className === 'color selected') {
+    e.target.style.backgroundColor = brown.style.backgroundColor;
+  }
+  if (yellow.className === 'color selected') {
+    e.target.style.backgroundColor = yellow.style.backgroundColor;
+  }
+  if (orange.className === 'color selected') {
+    e.target.style.backgroundColor = orange.style.backgroundColor;
+  }
+  
+}
+
+for (let i = 0; i < pixels.length; i += 1) {
+     pixels[i].addEventListener('click', preenche);
+}
+
+
+function limpaTudo() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
+btnLimpa.addEventListener('click', limpaTudo);
+
+// pixels.addEventListener('click'
